42dadb224c87c672f78533e352b721315c592d59
LeafyJade <dze3jz@virginia.edu>
2025-06-03 13:42:36 -0400

    Fix password variable

--- backend/app/db.py
+++ backend/app/db.py
@@ -10,7 +10,7 @@ def get_connection():
             host=os.getenv('DB_HOST'),
             port=int(os.getenv('DB_PORT')),
             user=os.getenv('DB_USER'),
-            password=os.getenv('DB_PASSWORD'),
+            password=os.getenv('DB_PASS'),
             database=os.getenv('DB_NAME')
         )
         return conn
